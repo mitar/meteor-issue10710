b2f5f794d9d4d862ad356bf704c75fbcd7fc2bc4
Mitar <mitar.git@tnode.com>
2019-09-23 16:16:08 -0700

    Enable CSP.

--- server/main.js
+++ server/main.js
@@ -1,3 +1,5 @@
+import {BrowserPolicy} from 'meteor/browser-policy';
 import {WebAppInternals} from 'meteor/webapp';
 
+BrowserPolicy.content.disallowInlineScripts();
 WebAppInternals.enableSubresourceIntegrity();
